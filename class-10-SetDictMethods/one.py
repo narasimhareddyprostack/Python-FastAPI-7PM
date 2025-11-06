99fcaa23342fd1f78242d7af806d4296fcf4af45
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-11-06 19:33:27 +0530

    tS

---
+++ class-10-SetDictMethods/one.py
@@ -0,0 +1,8 @@
+numbers=(11,7,18,31,232,1055,8,34,21)
+
+num_list=sorted(numbers)
+
+print(tuple(num_list))
+
+""" numbers.sort()   #NSO - ASC 
+print(numbers) """
---
+++ class-10-SetDictMethods/task2.py
@@ -0,0 +1,1 @@
+#print all product names
